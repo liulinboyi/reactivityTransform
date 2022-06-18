e2a65b54cc233d275ffd5b8aa035b710a7f5569f
liulinboyi <814921718@qq.com>
2022-06-18 23:18:18 +0800

    feat: init

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+let count = $ref(0);
+
+console.log(count);
+
+function increment() {
+  count++;
+}
+</script>
+
+<template>
+  <button @click="increment">{{ count }}</button>
+</template>
